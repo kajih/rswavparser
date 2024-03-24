2dac590aebdb1eee28c4af76bdda82bc87aeb624
KaJIH <kajih@mimiru>
2024-03-24 21:28:51 +0100

    fill out wav struct

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use std::{fs::File, io::Read};
 
 use nom::branch::alt;
 use nom::bytes::complete::tag;
-use nom::number::complete::le_u32;
+use nom::number::complete::{le_u16, le_u32};
 use nom::sequence::tuple;
 use nom::IResult;
 
@@ -15,7 +15,15 @@ struct Wav {
     file_len: u32,
     file_type: String,
     file_format: String,
-    chunk_len: u32,
+    header_len: u32,
+    data_type: u16,
+    channels: u16,
+    sample_rate: u16,
+    bitrate1: u32,
+    bitrate2: u32,
+    bits_sample: u16,
+    dataformat: String,
+    data_size: u32,
 }
 
 fn main() -> io::Result<()> {
@@ -62,15 +70,34 @@ fn parse_wave_chunk(input: &[u8]) -> IResult<&[u8], Wav> {
 
 fn parse_wave_fmt(input: &[u8]) -> IResult<&[u8], Wav> {
     let (input, _) = tag(b"fmt ")(input)?;
-    let (input, len) = le_u32(input)?;
+    let (input, parsed_data) = tuple((
+        le_u32, // Length of format data as listed above
+        le_u16, // Type of format (1 is PCM)
+        le_u16, // Number of channels
+        le_u16, // Sample Rate
+        le_u32, // (Sample rate * BPS * Channels)/8
+        le_u32, // (BitsPerSample * Channels) / 8.1
+        le_u16, // Bits per sample
+        tag(b"data"),
+        le_u32,
+    ))(input)?;
 
-    println!("len field {}", len);
+    println!("len field {}", parsed_data.0);
 
     Ok((
         input,
         Wav {
             file_format: "fmt ".to_string(),
-            chunk_len: len,
+            header_len: parsed_data.0,
+            data_type: parsed_data.1,
+            channels: parsed_data.2,
+            sample_rate: parsed_data.3,
+            bitrate1: parsed_data.4,
+            bitrate2: parsed_data.5,
+            bits_sample: parsed_data.6,
+            // dataformat: std::str::from_utf8(len.7).unwrap().to_string(),
+            dataformat: "data".to_string(),
+            data_size: parsed_data.8,
             ..Default::default()
         },
     ))
